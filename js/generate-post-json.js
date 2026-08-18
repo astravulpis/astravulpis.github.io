86e1f5e4854cab5eabcb52070164761d972b33d3
astravulpis <liam.berge72@gmail.com>
2026-08-18 16:49:54 +0200

    feat: implemented the parsing of the metadata of each post

--- js/generate-post-json.js
+++ js/generate-post-json.js
@@ -18,21 +18,63 @@ const existingPostsMap = new Map(existingPosts.map(post => [post.link, post]));
 const items = fs.readdirSync(targetDir, { withFileTypes: true });
 const postDirs = items.filter(item => item.isDirectory());
 
-const updatedPosts = postDirs.map(dir => {
-    const link = dir.name;
-    const existing = existingPostsMap.get(link);
 
-    return existing || {
-        link: link,
-        title: link.replace(/-/g, ' ').toUpperCase(),
-        description: "Placeholder",
-        rating: "★☆☆☆☆",
-        difficulty: "Easy",
-        tags: ["uncategorized"],
+const updatedPosts = [];
+
+postDirs.forEach(dir => {
+    const link = path.join(dir.name, '/');
+    const indexPath = path.join(targetDir, link, 'index.html');
+    const html = fs.readFileSync(indexPath, 'utf8');
+
+    const title    = retrieveTitle(html);
+    const desc     = retrieveMeta(html, 'description') || '';
+    const rating   = retrieveMeta(html, 'rating') || '★☆☆☆☆';
+    const diff     = retrieveMeta(html, 'difficulty') || 'N/A';
+    const tagsHtml = retrieveMeta(html, 'tags') || '';
+    const tags     = (tagsHtml)? tagsHtml.split(',').map(tag => tag.trim()) : [];
+    const timeData = retrieveDateInfo(html) || {
         dateFormatted: new Date().toLocaleDateString('en-US', {month: 'short', day: 'numeric', year: 'numeric' }),
         dateIso: new Date().toISOString().split('T')[0]
     };
+
+    updatedPosts.push({
+        link: link,
+        title: title,
+        description: desc,
+        rating: rating,
+        difficulty: diff,
+        tags: tags,
+        dateFormatted: timeData.dateFormatted,
+        dateIso: timeData.dateIso
+    });
 });
+
+function retrieveMeta(html, name) {
+    const regex = new RegExp(`<meta\\s+name=["']${name}["']\\s+content=["'](.*?)["']`, 'i');
+    const match = html.match(regex)
+    return (match)? match[1] : null;
+}
+
+function retrieveTitle(html) {
+    const regex = /<title>(.*?)<\/title>/i;
+    const match = html.match(regex)
+    return (match)? match[1] : null;
+}
+
+function retrieveDateInfo(html) {
+    const regex = /<time\s+datetime=["'](.*?)["']>(.*?)<\/time>/i;
+    const match = html.match(regex)
+    if (match) {
+        return {
+            dateIso: match[1],
+            dateFormatted: match[2],
+        };
+    }
+    return null;
+}
+
+updatedPosts.sort((a, b) => new Date(b.dateIso) - new Date(a.dateIso));
+// console.log(updatedPosts);
 
 fs.writeFileSync(outputJSON, JSON.stringify(updatedPosts, null, 2));
 console.log(`Generated blog/posts.json with ${updatedPosts.length} posts.`);
